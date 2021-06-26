9b30ebeea43c327b16a7f897d560eb686a0c2622
Nurlan Baitleuov <83259656+nurlan-cell@users.noreply.github.com>
2021-06-26 08:11:29 +0600

    Add files via upload

---
+++ Testing/script.js
@@ -0,0 +1,29 @@
+//Год Рождения
+$(document).ready(function() {
+    $("select").change(function() {
+        $(this).find("option").html(function(){ return this.value + " " + $(this).attr("data-count"); });
+        $("option:selected",this).html(this.value);
+    }).change();
+});
+
+
+//при ширине окна браузера менее 768px меню нужно сделать выпадающим 
+$("<select />").appendTo("navbar");
+
+$("<option />", {
+   "selected": "selected",
+   "value"   : "",
+   "text"    : "Go to..."
+}).appendTo("nav select");
+
+$("navbar a").each(function() {
+ var el = $(this);
+ $("<option />", {
+     "value"   : el.attr("href"),
+     "text"    : el.text()
+ }).appendTo("navbar select");
+});
+
+$("navbar select").change(function() {
+    window.location = $(this).find("option:selected").val();
+  });
